45abb395053e7221b192723af9a00ff9cf6af109
Brian <120551412+DnKhanhh@users.noreply.github.com>
2024-05-02 11:28:04 +0700

    update block package

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/briandnk/simple-blockchain/blockchain"
+	"strconv"
 )
 
 func main() {
@@ -17,5 +18,9 @@ func main() {
 		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
 		fmt.Printf("Data in Block: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
+
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
 	}
 }
